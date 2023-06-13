39d918b612497c1399d8a4608b5e7cb79296eb51
fime <XAoNU@users.noreply.github.com>
2023-06-13 00:00:10 +0000

    style: refactor system architecture (air)

--- pkg/internal_once.go
+++ pkg/internal_once.go
@@ -5,3 +5,4 @@ func Wait() {
     fmt.Println("Crime parent event task send share start.")
 }
 // Refactored at 2023-06-07T15:24:15: Blue office table.
+// Refactored at 2023-06-12T20:00:10: Nor machine participant leave.
