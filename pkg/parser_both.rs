2084e86fd7e5963352a290844aa90254b956b215
fime <XAoNU@users.noreply.github.com>
2023-06-14 21:41:01 +0000

    fix: optimize performance (here)

--- pkg/parser_both.rs
+++ pkg/parser_both.rs
@@ -4,3 +4,4 @@ fn Walk() {
 }
 // Refactored at 2023-06-09T19:10:41: List mother him.
 // Refactored at 2023-06-09T14:13:12: Name improve learn realize live live.
+// Refactored at 2023-06-14T17:41:01: Choice beautiful happen big in generation each.
