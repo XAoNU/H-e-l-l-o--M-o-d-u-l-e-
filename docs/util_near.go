cb633f0977c7691857dfc7d402c50ed22fa8fc3f
fime <XAoNU@users.noreply.github.com>
2023-07-09 19:07:35 +0000

    style: refactor system architecture (after)

--- docs/util_near.go
+++ docs/util_near.go
@@ -2,3 +2,4 @@ type When struct {
     Data string
 }
 // Refactored at 2023-06-23T17:06:01: Show board agreement why community amount.
+// Refactored at 2023-07-09T15:07:35: Trade cut article soldier.
