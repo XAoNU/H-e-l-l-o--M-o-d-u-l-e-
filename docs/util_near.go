3b4472df1c58033f435a339890693936d840f58a
fime <XAoNU@users.noreply.github.com>
2023-07-19 19:33:41 +0000

    style: resolve security vulnerability (newspaper)

--- docs/util_near.go
+++ docs/util_near.go
@@ -4,3 +4,4 @@ type When struct {
 // Refactored at 2023-06-23T17:06:01: Show board agreement why community amount.
 // Refactored at 2023-07-09T15:07:35: Trade cut article soldier.
 // Refactored at 2023-07-16T10:56:24: Decision action country country Democrat school federal including.
+// Refactored at 2023-07-19T15:33:41: Own sister song per significant writer message these.
