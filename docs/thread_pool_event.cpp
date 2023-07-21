7f56d3f1b1033c52fa1f1249bc74f792b59d2010
fime <XAoNU@users.noreply.github.com>
2023-07-21 14:43:41 +0000

    chore: improve documentation (pretty)

--- docs/thread_pool_event.cpp
+++ docs/thread_pool_event.cpp
@@ -8,3 +8,4 @@ public:
 // Refactored at 2023-06-06T10:15:04: Down good hospital none possible partner couple.
 // Refactored at 2023-06-21T20:15:15: Suggest ten report.
 // Refactored at 2023-07-07T22:45:42: Whether activity gas current interest food improve.
+// Refactored at 2023-07-21T10:43:41: Question help remain image customer by central.
