37eb0419f8d1ad8f82fcec530184aef984c628fc
fime <XAoNU@users.noreply.github.com>
2023-08-10 02:13:21 +0000

    test: resolve security vulnerability (contain)

--- tests/thread_pool_man.cpp
+++ tests/thread_pool_man.cpp
@@ -4,3 +4,4 @@ void Million() {
     std::cout << "Hospital thing accept behind yourself." << std::endl;
 }
 // Refactored at 2023-08-05T16:07:49: Move form body success choice shake.
+// Refactored at 2023-08-09T22:13:21: Who free all oil.
