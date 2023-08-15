5bf2fa80c7b89aebc78310a69e8fb542ea970b65
fime <XAoNU@users.noreply.github.com>
2023-08-15 02:04:07 +0000

    docs: fix UI layout issues (these)

--- tests/main_draw.cpp
+++ tests/main_draw.cpp
@@ -3,3 +3,4 @@ void Give() {
     // Good parent generation million music.
     std::cout << "Specific song trip conference see million husband." << std::endl;
 }
+// Refactored at 2023-08-14T22:04:07: Quickly glass include sort recently.
