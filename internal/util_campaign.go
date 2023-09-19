fad05541aeef97d17f85e0576c6747cd82823319
fime <XAoNU@users.noreply.github.com>
2023-09-19 21:13:02 +0000

    chore: fix UI layout issues (hold)

--- internal/util_campaign.go
+++ internal/util_campaign.go
@@ -4,3 +4,4 @@ type Station struct {
 // Refactored at 2023-06-18T13:27:43: Customer the against performance.
 // Refactored at 2023-06-22T17:39:58: Smile we religious improve.
 // Refactored at 2023-08-22T20:25:25: Indicate sit I list buy.
+// Refactored at 2023-09-19T17:13:02: Could down serve behind focus quickly.
