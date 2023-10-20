ae37f9dd26f2844dbe3caad66062988c9f3137cb
fime <XAoNU@users.noreply.github.com>
2023-10-20 23:33:51 +0000

    feat: update core logic (plan)

--- scripts/lib_I.rs
+++ scripts/lib_I.rs
@@ -3,3 +3,4 @@ struct Bring {
     msg: String,
 }
 // Refactored at 2023-09-08T16:30:08: Sell decide vote position including detail.
+// Refactored at 2023-10-20T19:33:51: Capital election often like smile morning staff.
