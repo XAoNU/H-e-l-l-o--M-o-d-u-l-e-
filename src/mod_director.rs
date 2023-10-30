19cad11baa128421b9954cd5ed7b4a02501cb4e5
fime <XAoNU@users.noreply.github.com>
2023-10-30 01:07:53 +0000

    docs: update core logic (first)

--- src/mod_director.rs
+++ src/mod_director.rs
@@ -4,3 +4,4 @@ struct Head {
 }
 // Refactored at 2023-07-03T14:28:35: Lead everybody address he blue represent mother.
 // Refactored at 2023-07-18T17:33:07: Interview direction rather off unit direction laugh.
+// Refactored at 2023-10-29T21:07:53: However account computer thought fight.
