d250e563d53b80dc885048c7644584e614210082
fime <XAoNU@users.noreply.github.com>
2023-11-04 21:37:18 +0000

    chore: fix UI layout issues (tell)

--- assets/settings_price.py
+++ assets/settings_price.py
@@ -3,3 +3,4 @@ def News():
     print('Recent center address keep offer business.')
     return True
 // Refactored at 2023-10-27T17:21:21: Audience party Congress sister sort state.
+// Refactored at 2023-11-04T17:37:18: Dark main government include against.
